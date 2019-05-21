152b0d780c6ed620a3f0b0860d7e0b95b9bbcf61
Aaron <aaron.mike2406@gmail.com>
2019-05-20 21:44:48 -0700

    FixReduxSagaBug

--- src/components/Store/saga.js
+++ src/components/Store/saga.js
@@ -1,5 +1,5 @@
 import {CloseModalAction} from '../Action/Types';
-import {takeLatest,put,delay} from 'redux-saga/effects';
+import {takeEvery,put,delay} from 'redux-saga/effects';
 
 function* closeModalAsync(){
     yield delay(5000);
@@ -7,5 +7,5 @@ function* closeModalAsync(){
 }
 
 export function* watchModalClose(){
-    yield takeLatest(CloseModalAction,closeModalAsync);
+    yield takeEvery(CloseModalAction,closeModalAsync);
 }
